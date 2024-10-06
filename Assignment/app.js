458c4138c0df39592f4964880309a699223b627d
MEHAK MEHTAB <mehakmehtab01@gmail.com>
2024-10-06 19:52:43 +0500

    Add files via upload

---
+++ Assignment/app.js
@@ -0,0 +1,20 @@
+//Assignment of Chapter 01 Js 
+
+//Question no 01 ?
+// Write a script to greet your website visitor using JS alert box.
+// alert("Welcome to our website! We're glad to have you here.");
+
+//Question no 02 ?
+//alert("Error! Please enter a valid password");
+
+//Question no 03 ?
+// alert("Welcome to JS Land... \n Happy Coding!");
+
+//Question no 04 ?
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!  \n ⬛    Prevent this page from creating additional dialogs");
+
+//Question no 04 ?
+// console.log(alert("Hello...   I can run JS through my web browser's  console")  );
+
+
